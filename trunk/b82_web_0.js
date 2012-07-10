49e05cf72caf96564c2404299b96f0532254ec75
webmaster@b82.dk <webmaster@b82.dk@728826cd-e99b-93c2-0458-0c46fc220363>
2012-07-10 18:36:04 +0000

    debug

--- trunk/b82_web_0.js
+++ trunk/b82_web_0.js
@@ -70,8 +70,9 @@ function show_webshop() {
   var script = document.createElement('script');
   document.write('debug3a');
 
-  script.setAttribute('src','https://spreadsheets.google.com/feeds/list/0AjVUzy2DQAb0dEgxUjlKdVI5RHNGNVIzZlVDd0pQYnc/1/public/values?alt=json-in-script&callback=show_products');
-  script.setAttribute('type','text/javascript');
+//  script.setAttribute('src','https://spreadsheets.google.com/feeds/list/0AjVUzy2DQAb0dEgxUjlKdVI5RHNGNVIzZlVDd0pQYnc/1/public/values?alt=json-in-script&callback=show_products');
+//  script.setAttribute('type','text/javascript');
+  script.src='https://spreadsheets.google.com/feeds/list/0AjVUzy2DQAb0dEgxUjlKdVI5RHNGNVIzZlVDd0pQYnc/1/public/values?alt=json-in-script&callback=show_products';
   
   document.write('debug1');
   document.body.appendChild(script);
