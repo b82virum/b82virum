e62f15c2e5bef66730437f378f0c0b4567a6b197
webmaster@b82.dk <webmaster@b82.dk@728826cd-e99b-93c2-0458-0c46fc220363>
2012-07-10 21:00:39 +0000

    new gid

--- trunk/b82_web_0.js
+++ trunk/b82_web_0.js
@@ -66,6 +66,6 @@ function show_products(json) {
 
 function show_webshop() {
 
-  document.write('<script src="https://spreadsheets.google.com/feeds/list/0Akm30OX8lPv2dEVvZ2xWaE5Zb3VnbUFwRU1hQWN0MHc/1/public/values?alt=json-in-script&callback=show_products" type="text/javascript"></script>');
+  document.write('<script src="https://spreadsheets.google.com/feeds/list/0Akm30OX8lPv2dEVvZ2xWaE5Zb3VnbUFwRU1hQWN0MHc/2/public/values?alt=json-in-script&callback=show_products" type="text/javascript"></script>');
 
 }
