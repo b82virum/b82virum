6da9c6c9cbf717d71c16b09f438b13f6bf2acd0e
webmaster@b82.dk <webmaster@b82.dk@728826cd-e99b-93c2-0458-0c46fc220363>
2012-07-17 16:01:41 +0000

    pagestart

--- trunk/b82_web_0.js
+++ trunk/b82_web_0.js
@@ -108,19 +108,19 @@ function show_webshop() {
 function page_start() {
 
   document.write(''
-    + 'hellostyle<style type="text/css">'
+    + 'hellostyle'
+    + '<style type="text/css">'
     + '<!--'
     + '.blogger-post-footer {'
     + 'visibility: hidden;'
     + '}'
-    + '--></style>'
-    + '<!-- Fjern feed2js reklame -->'
+    + '</style>'
     + '<style type="text/css">'
     + '<!--'
     + 'li.f2jnag {'
     + ' display:none;'
     + '}'
-    + '--></style>'
+    + '</style>'
   );
 
 }
