67f36a1b7d9c4d03cb1d9851253dff6cb3fc3aed
webmaster <webmaster@b82.dk>
2012-07-18 15:12:00 +0000

    logo

--- b82_web_0.js
+++ b82_web_0.js
@@ -121,7 +121,7 @@ function page_start() {
   );
 
   document.write(''
-    + '<div style="position: fixed; left: 0px; top: 0px; z-index: 255;">'
+    + '<div style="position: fixed; left: 10px; top: 10px; z-index: 255;">'
     + '<img style="border-style: none; background-color: transparent;" border="0" src="https://lh6.googleusercontent.com/-viTn7BuAhK8/T_C5WooraDI/AAAAAAAAC3M/QhybnVT1HBM/s250/Logo.RedbaseWhitebackTrans.png"/>'
     + '</div>'
   );
