10647b148adab85298465408383506fa858561d3
webmaster <webmaster@b82.dk>
2012-09-05 21:07:25 +0000

    logo size

--- b82_web_0.js
+++ b82_web_0.js
@@ -340,6 +340,19 @@ function show_graphic(label) {
 
 }
 
+function imgsize600(x)
+{
+x.style.height="600px";
+x.style.width="600px";
+}
+
+function imgsize150(x)
+{
+x.style.height="150px";
+x.style.width="150px";
+}
+
+
 function page_start() {
 
   document.write(''
@@ -362,21 +375,9 @@ function page_start() {
     + '</style>'
   );
 
-function imgsize600(x)
-{
-x.style.height="600px";
-x.style.width="600px";
-}
-
-function imgsize150(x)
-{
-x.style.height="150px";
-x.style.width="150px";
-}
-
   document.write(''
     + '<div style="position: fixed; left: 50%; margin-left: -480px; top: 0px; z-index: 255;">'
-    + '<img onmouseover="imgsize600(this)" onmouseout="imgsize150(this)" style="border-style: none; background-color: transparent;" border="0" src="https://lh6.googleusercontent.com/-viTn7BuAhK8/T_C5WooraDI/AAAAAAAAC3M/QhybnVT1HBM/s150/Logo.RedbaseWhitebackTrans.png"/>'
+    + 'xxx<img onmouseover="imgsize600(this)" onmouseout="imgsize150(this)" style="border-style: none; background-color: transparent;" border="0" src="https://lh6.googleusercontent.com/-viTn7BuAhK8/T_C5WooraDI/AAAAAAAAC3M/QhybnVT1HBM/s150/Logo.RedbaseWhitebackTrans.png"/>'
     + '</div>'
   );
   
