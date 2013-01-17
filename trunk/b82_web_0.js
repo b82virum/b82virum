14362afbef451c4ee9cfde8f1b881d74ccd76690
webmaster@b82.dk <webmaster@b82.dk@728826cd-e99b-93c2-0458-0c46fc220363>
2013-01-17 19:14:22 +0000

    logo

--- trunk/b82_web_0.js
+++ trunk/b82_web_0.js
@@ -396,10 +396,15 @@ function page_start() {
     + '</style>'
   );
 
+/*
   document.write(''
-    + 'xxx2<div id="b82logo" style="position: fixed; left: 50%; margin-left: -485px; top: 0px; z-index: 255;">'
+    + '<div id="b82logo" style="position: fixed; left: 50%; margin-left: -485px; top: 0px; z-index: 255;">'
     + '<img width="960px" height="960px" style="border-style: none; background-color: transparent;" border="0" src="https://lh6.googleusercontent.com/-viTn7BuAhK8/T_C5WooraDI/AAAAAAAAC3M/QhybnVT1HBM/s960/Logo.RedbaseWhitebackTrans.png"/>'
     + '</div>'
+  );
+*/
+  document.write(''
+    + 'zzz1<img id="b82logo" width="960px" height="960px" style="position: fixed; left: 50%; margin-left: -485px; top: 0px; z-index: 255; border-style: none; background-color: transparent;" border="0" src="https://lh6.googleusercontent.com/-viTn7BuAhK8/T_C5WooraDI/AAAAAAAAC3M/QhybnVT1HBM/s960/Logo.RedbaseWhitebackTrans.png"/>'
   );
   
   show_header('B82%20Header');
