77a27134327112f77c665651ba29ad4c7c816e81
webmaster@b82.dk <webmaster@b82.dk@728826cd-e99b-93c2-0458-0c46fc220363>
2013-02-13 21:11:18 +0000

    d

--- trunk/b82_web_1.js
+++ trunk/b82_web_1.js
@@ -14,7 +14,42 @@ function feed(label,max) {
   return r;
 }
 
+
+
 function test_feed_blog() {
 }
 function test_feed_label() {
+}
+
+function test_feed(label,max) {
+
+  $.ajax({
+    url: feed(label,max),
+    type: 'get',
+    dataType: 'jsonp',
+    success: function(data) {
+      var posturl = "";
+      var htmlcode = '<ul>';
+      for (var i = 0; i < data.feed.entry.length; i++) {
+        for (var j=0; j < data.feed.entry[i].link.length; j++) {
+          if (data.feed.entry[i].link[j].rel == "alternate") {
+            posturl = data.feed.entry[i].link[j].href;
+            break;
+          }
+        }
+        if ("content" in data.feed.entry[i]) {
+          var postcontent = data.feed.entry[i].content.$t;
+        } else if ("summary" in data.feed.entry[i]) {
+          var postcontent = data.feed.entry[i].summary.$t;
+        } else {
+          var postcontent = "";
+        }
+        var posttitle = data.feed.entry[i].title.$t;
+        htmlcode += '<li><div><a href="'+posturl+'" target="_blank">'+posttitle+'</a></div><div>'+postcontent+'</div></li>';
+      }
+      htmlcode += '</ul>';
+      document.getElementById('result').innerHTML = htmlcode;
+    }
+  });
+
 }
