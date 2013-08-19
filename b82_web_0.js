03e94b3f061cc2c4702d29138b2d82fde1371e07
MortenKjarulff@gmail.com <MortenKjarulff@gmail.com>
2013-08-19 08:43:29 +0000

    mp

--- b82_web_0.js
+++ b82_web_0.js
@@ -392,7 +392,7 @@ function show_graphic(label) {
 
 function page_start() {
 
-  document.write('bbb'
+  document.write('ccc'
     + '<style type="text/css">'
     + '.blogger-post-footer {'
     + '  visibility: hidden;'
@@ -409,7 +409,7 @@ function page_start() {
     + 'h3, h4, h5, h6 {'
     + '  color: black;'
     + '}'
-    + 'p.mobile-photo {'
+    + 'full-width-content.p.mobile-photo {'
     + '  text-align: center; important!'
     + '}'
     + '</style>'
