8581c018cbace046d28ea0f450d188629bfb917c
webmaster <webmaster@b82.dk>
2013-09-10 19:27:02 +0000

    vc

--- b82_web_1.js
+++ b82_web_1.js
@@ -156,7 +156,7 @@ function show_blog_feed(div,labels,max,random,header,show_title,show_content,sho
             
           }
           
-          html += '<p>bbb{' + name + '}' + content + '</p>';
+          html += '<p>' + content + '</p>';
         }  
 
         html += '</div>';
