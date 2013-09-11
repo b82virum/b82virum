53222160626917578f3bea217f9c562b239dc7bf
webmaster@b82.dk <webmaster@b82.dk@728826cd-e99b-93c2-0458-0c46fc220363>
2013-09-11 18:53:46 +0000

    vc

--- trunk/b82_web_1.js
+++ trunk/b82_web_1.js
@@ -150,7 +150,7 @@ function show_blog_feed(div,labels,max,random,header,show_title,show_content,sho
               n = content.indexOf('>',n);
               if (n != -1) {
                 n++;
-                //content = content.slice(0,n) + vcard + content.slice(n);
+                content = content.slice(0,n) + vcard + content.slice(n);
               }
             }
             
