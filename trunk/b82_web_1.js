a1e72ec86faf2aa668d30d867888b266e474113f
MortenKjarulff@gmail.com <MortenKjarulff@gmail.com@728826cd-e99b-93c2-0458-0c46fc220363>
2014-01-13 15:24:57 +0000

    img

--- trunk/b82_web_1.js
+++ trunk/b82_web_1.js
@@ -284,7 +284,7 @@ function page_start(div) {
   var ndiv;
 
   $('#'+div).append(''
-    + '<style type="text/css">'
+    + 'aaa<style type="text/css">'
     + '.blogger-post-footer {'
     + '  visibility: hidden;'
     + '}'
@@ -306,6 +306,7 @@ function page_start(div) {
     + '.noprint, #slideshow-wrapper, #menu-wrapper, #copyright {display:none !important;}'
     + '#main {border-style:none !important;}'
     + '}'
+    + 'img {height: auto; max-height: auto; width: auto; max-width: 50%;}'
     + '</style>'
   );
 
