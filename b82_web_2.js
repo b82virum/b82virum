bf5f8a5bdd7f78cc76d3e08ade0414dbd887b1f9
b82virum <webmaster@b82.dk>
2016-02-03 16:57:42 +0100

    Update b82_web_2.js

--- b82_web_2.js
+++ b82_web_2.js
@@ -740,13 +740,13 @@ function show_join(div,label) {
 
         if (data.feed.entry[i].gsx$team.$t == 'dbu') {
           html += '<p><div style="text-align:center;">';
-          html += 'På dette hold bruger vi kluboffice til medlemsregistrering og kontingentopkrævning. ';
+          html += 'På dette hold bruger vi <b>kluboffice</b> til medlemsregistrering og kontingentopkrævning. ';
           html += '</div></p>';
           html += '<h3><a href="http://+++">Klik her for at tilmelde dig</a></h3><p></p>';
         }
         else {
           html += '<p><div style="text-align:center;">';
-          html += 'På dette hold bruger vi holdsport til medlemsregistrering og kontingentopkrævning. ';
+          html += 'På dette hold bruger vi <b>holdsport</b> til medlemsregistrering og kontingentopkrævning. ';
           html += '</div></p>';
           html += '<h3><a href="http://www.holdsport.dk/klub/b82virum">Klik her for at tilmelde dig</a></h3><p></p>';
         }
